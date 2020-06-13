74bd4847bec8afb32d1e3e315d02b1b8accdb5a3
RyanAquino <ryan.aquino7110@gmail.com>
2020-06-13 16:54:47 +0800

    cleanup imgs | set mobile first design

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 console.log('Attached');
+
+function hideMenu(){
+	let menuOpen = document.querySelector('.toggler').checked;
+
+	if(menuOpen = true){
+		document.querySelector('.toggler').checked = false;
+	}
+}
+
+window.addEventListener("scroll", hideMenu);
