76fa22c27dea3742b3453ae073e1e50c464a36dc
RyanAquino <ryan.aquino7110@gmail.com>
2020-06-16 19:45:34 +0800

    Added initial content for about | contact | div cards on projects section

--- js/script.js
+++ js/script.js
@@ -2,10 +2,25 @@ console.log('Attached');
 
 function hideMenu(){
 	let menuOpen = document.querySelector('.toggler').checked;
-
 	if(menuOpen = true){
 		document.querySelector('.toggler').checked = false;
 	}
 }
 
 window.addEventListener("scroll", hideMenu);
+
+
+
+// Smooth scroll ios
+// let viewWorkBtn = document.querySelector('#btn');
+
+// function scrollTo() {
+// 	let projectSection = document.querySelector('#projects');
+// 	projectSection.scrollIntoView({behavior: "smooth"});
+
+// }
+// viewWorkBtn.addEventListener('click', scrollTo);
+
+
+
+
