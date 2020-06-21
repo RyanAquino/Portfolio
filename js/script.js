85811c34159264b0d2a94acbdf537b9fba2e6970
RyanAquino <ryan.aquino7110@gmail.com>
2020-06-21 09:15:31 +0800

    separate imgs to device sizes | added styles on media queries

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
-console.log('Attached');
+let project1 = document.querySelector('#project-1');
+let project2 = document.querySelector('#project-2');
+let project3 = document.querySelector('#project-3');
+let project4 = document.querySelector('#project-4');
+let project5 = document.querySelector('#project-5');
+
+
+
+window.addEventListener('scroll', hideMenu);
+
+project4.addEventListener('click', goToURL);
+project4.url = 'http://laravelblogapp.herokuapp.com/';
+
+project5.addEventListener('click', goToURL);
+project5.url = 'https://www.facebook.com/ParkInLotPH/';
+
+
+function goToURL(evt) {
+	let url = evt.currentTarget.url;
+	window.open(url)
+}
 
 function hideMenu(){
 	let menuOpen = document.querySelector('.toggler').checked;
@@ -6,6 +26,3 @@ function hideMenu(){
 		document.querySelector('.toggler').checked = false;
 	}
 }
-
-window.addEventListener("scroll", hideMenu);
-
