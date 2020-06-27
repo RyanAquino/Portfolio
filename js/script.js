8c367d84cbbf97298c59bbc34b6c3899141824b3
RyanAquino <ryan.aquino7110@gmail.com>
2020-06-27 10:44:41 +0800

    Added view work button smooth scroll support for IOS

--- js/script.js
+++ js/script.js
@@ -3,16 +3,52 @@ let project2 = document.querySelector('#project-2');
 let project3 = document.querySelector('#project-3');
 let project4 = document.querySelector('#project-4');
 let project5 = document.querySelector('#project-5');
+let viewWorkBtn = document.querySelector('#btn');
 
+// let homeMenu = document.querySelector('.home');
+// let aboutMenu = document.querySelector('.about');
+// let projectsMenu = document.querySelector('.projects');
+// let contactMenu = document.querySelector('.contact');
+
+// menu and button smooth scroll ios support
+if(/iPad|iPhone|iPod/.test(navigator.userAgent)) {
+	viewWorkBtn.addEventListener('click', (event) => iosSmoothScroll(event,'.content-4'));
+	// homeMenu.addEventListener('click', (event) => iosSmoothScroll(event,'.content-1'));
+	// aboutMenu.addEventListener('click', (event) => iosSmoothScroll(event,'.content-2'));
+	// projectsMenu.addEventListener('click', (event) => iosSmoothScroll(event,'.content-4'));
+	// contactMenu.addEventListener('click', (event) => iosSmoothScroll(event,'.content-5'));
+}
+
+// scroll events
 window.addEventListener('scroll', hideMenu);
 window.addEventListener('scroll', showProjects);
 window.addEventListener('scroll', showSkills);
 
+// project urls
 project4.addEventListener('click', goToURL);
 project4.url = 'http://laravelblogapp.herokuapp.com/';
 
 project5.addEventListener('click', goToURL);
 project5.url = 'https://www.facebook.com/ParkInLotPH/';
+
+
+// functions
+function iosSmoothScroll(e, section){
+	e.preventDefault();
+	let position = 0;
+
+	let sectionEl = document.querySelector(section);
+	let projectsPos = sectionEl.getBoundingClientRect().top / 1.050;
+
+	let scroll = setInterval(() => {
+		position += 15;
+		window.scrollTo(0, position);
+
+		if(position >= projectsPos){
+			clearInterval(scroll);
+		}
+	},1);
+}
 
 
 function goToURL(evt) {
